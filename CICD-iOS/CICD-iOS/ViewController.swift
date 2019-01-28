8fd6b61f40c60361308450aac207059697e6e931
Rafat Touqir Rafsun <archeco-511@Rafats-MacBook-Pro.local>
2019-01-28 14:07:37 +0600

    First Commit

---
+++ CICD-iOS/CICD-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CICD-iOS
+//
+//  Created by Rafat Touqir Rafsun on 1/28/19.
+//  Copyright © 2019 Rafat Touqir Rafsun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CICD-iOS/CICD-iOSTests/CICD_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  CICD_iOSTests.swift
+//  CICD-iOSTests
+//
+//  Created by Rafat Touqir Rafsun on 1/28/19.
+//  Copyright © 2019 Rafat Touqir Rafsun. All rights reserved.
+//
+
+import XCTest
+@testable import CICD_iOS
+
+class CICD_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssert(1==1)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
